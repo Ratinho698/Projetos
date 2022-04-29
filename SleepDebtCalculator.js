e0c5806b6a9fd74f4b24e3fd1289dc946560a692
Ratinho698 <103133474+Ratinho698@users.noreply.github.com>
2022-04-29 16:28:12 +0100

    Add files via upload

---
+++ SleepDebtCalculator.js
@@ -0,0 +1,42 @@
+const getSleepHours = day => {
+    switch (day){
+      case 'monday':
+        return 8;
+        break;
+      case 'tuesday':
+        return 6;
+        break;
+      case 'wednesday':
+        return 7;
+        break;
+      case 'thursday':
+        return 8;
+        break;
+      case 'friday':
+        return 5;
+        break;
+      case 'saturday':
+        return 4;
+        break;
+      case 'sunday':
+        return 9;
+        break;
+    }
+  }
+  const getActualSleepHours = () => getSleepHours('monday') + getSleepHours('tuesday') + getSleepHours('wednesday') + getSleepHours('thursday') + getSleepHours('friday') + getSleepHours('saturday') + getSleepHours('sunday');
+  const getIdealSleepHours = () => {
+    const idealHours = 8;
+    return idealHours * 7;
+  }
+  const calculateSleepDebt = () => {
+    const actualSleepHours = getActualSleepHours();
+    const idealSleepHours = getIdealSleepHours();
+    if (actualSleepHours === idealSleepHours){
+      console.log('You got the perfect amount of sleep this week!');
+    } else if (actualSleepHours > idealSleepHours){
+      console.log(`You got ${actualSleepHours - idealSleepHours} extra hour(s) of sleep this week. You got more sleep than needed!`);
+    } else {
+      console.log(`You got ${idealSleepHours - actualSleepHours} hour(s) less of sleep than you needed this week. You should rest more!`);
+    }
+  }
+  calculateSleepDebt();
